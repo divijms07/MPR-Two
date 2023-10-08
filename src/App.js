1459d4a5e91a37c3e97e563ab683e45bf2f698d5
divijms07 <divijsarkale@gmail.com>
2023-10-08 22:59:29 +0530

    MPR-Two

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Contact from "./components/Contact";
+import EmbeddedMap from './components/EmbeddedMap'; 
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <Contact />
+
+      <EmbeddedMap />
+
+    </div>
+  );
+}
+
+export default App;
